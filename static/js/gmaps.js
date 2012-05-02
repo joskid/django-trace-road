82a40bad000dcfe80b4d9200452300dcc0b99a80
Alex Schemelev <alex.schemelev@gmail.com>
2012-05-02 18:14:55 +0300

    refactored logic

--- static/js/gmaps.js
+++ static/js/gmaps.js
@@ -1,23 +1,27 @@
-(function(){
-     window.GMapsApp = Backbone.View.extend(
-         {
-             initialize: function(){
-                 this.map = this.init_map(this.options.center);
-             },
+//    Displays Google map with a given 'center[lat, lng]' and 'zoom'.
 
+(function () {
+     window.GMapsApp = Backbone.View.extend({
+         
+         // you should provide 'el' like '#map_canvas' to attach Google Map to a required HTML object.
+         initialize: function () {
+             this.map = this.initGoogleMap();
+         },
 
-             init_map: function(center){
-                 var map_options = {
-		     zoom: 15,
-                     center: new google.maps.LatLng(center[0], center[1]),
-		     mapTypeId: google.maps.MapTypeId.HYBRID,
-		     mapTypeIds: [google.maps.MapTypeId.HYBRID]
-                 };
-                 var map = new google.maps.Map(document.getElementById('map_canvas'), map_options);
-                 var bikeLayer = new google.maps.BicyclingLayer();
-                 bikeLayer.setMap(map);
-                 return map;
-             }
-         });
+         initGoogleMap: function () {
+             var mapOptions = {
+		 zoom: this.options.zoom || 15,
+                 center: new google.maps.LatLng(this.options.center[0], this.options.center[1]),
+		 mapTypeId: google.maps.MapTypeId.HYBRID,
+		 mapTypeIds: [google.maps.MapTypeId.HYBRID]
+             };
+             var map = new google.maps.Map(this.el[0], mapOptions);
+             var bikeLayer = new google.maps.BicyclingLayer();
+             bikeLayer.setMap(map);
+             return map;
+         }
+
+    });
      
+
 })();
--- static/js/traces.js
+++ static/js/traces.js
@@ -1,33 +1,18 @@
-(function(){
-
-     var Trace = Backbone.Model.extend(
-         {
-             urlRoot: '/traces/'
-         });
+// Quick and dirty solution for GPS tracing system.
+// It stores Points in local storage and PUTs Trace object with points to a server only on 'stop/pause' events. 
+// That happens if user clicks 'stop' button or leaves the window.
 
 
-     var Point = Backbone.Model.extend(
-         {
-         });
-
-    
-     var Traces = Backbone.Collection.extend({
-             model: Trace,
-             url: function() {
-                 return '/traces/';
-             },
-
-             initialize: function(options) {
-                 this.options = options;
-             }                                                    
-         }
-     );
-
-
+(function () {
+     // Server-side Trace instance. The stucture: trace['points']
+     var Trace = Backbone.Model.extend({urlRoot: '/traces/'});
+     
+     // Point and colletion of Points are working with local storage.
+     var Point = Backbone.Model.extend({});    
      var Points = Backbone.Collection.extend({
              model: Point,
              localStorage: new Backbone.LocalStorage("PointCollection"),
-             initialize: function(options) {
+             initialize: function (options) {
                  this.options = options;
              }                                                    
          }
@@ -40,8 +25,8 @@
                  "click a": "toggle_record"
              },
 
-             toggle_record: function(){
-                 if (this.status == 'off'){
+             toggle_record: function () {
+                 if (this.status === 'off') {
                      this.status = 'on';
                      this.start_record();
                      this.el.find('span').text("pause || stop");
@@ -52,63 +37,61 @@
                      this.el.find('span').text("resume");
                      this.toggle_color(this.el.find('a'), '#FFC300', '#E84C38');
                  }
-                 
                  return false;
              },
 
 
-             toggle_color: function(a, back, hover) {
+             toggle_color: function (a, back, hover) {
                  return a.css({background: back}).hover(
-                     function(){
+                     function () {
                          $(this).css('background', hover);
                      },
-                     function(){
+                     function () {
                          $(this).css('background', back);
                      });
              },
 
-
-             initialize: function() {
+             initialize: function () {
                  var self = this;
                  this.el = $(this.el);
                  this.status = 'off';
                  //this.collection.bind('add', this.track_change);
-                 this.map = window.gmaps.map;
+                 this.map = this.options.map;
                  this.geocoder = new google.maps.Geocoder();
                  this.polyline = new google.maps.Polyline({map: this.map, path: []});
                  this.infowindow = new google.maps.InfoWindow();
-                 
-                 $(window).bind('beforeunload', function() {
-                                    if (this.status == 'on'){
-                                        self.toggle_record();
-                                    }
-                                }); 
+                 // do not lose results on window 'unload'.
+                 $(window).bind('beforeunload', function () {
+                     if (this.status == 'on'){
+                         self.toggle_record();
+                     }
+                 }); 
              },                                               
              
-
-             track_change: function() {
+             track_change: function () {
+                //TODO: add info window logic.
              },
 
-
-             renderInfoWindow: function(model) {
+             renderInfoWindow: function (model) {
                  var self = this;
-                 var location = new google.maps.LatLng(
-                     model.get('lat'),  model.get('lng'));
-                 var marker = new google.maps.Marker(
-                     {
+                 var location = new google.maps.LatLng(model.get('lat'),  model.get('lng'));
+                 var marker = new google.maps.Marker({
                          map: this.map,
                          position: location
                      });
+
+                 // geocode the location to make it human readable.
                  this.geocoder.geocode({
                          'latLng': location
                      }, 
-                     function(results, status) {
-                         if (status == google.maps.GeocoderStatus.OK) {
-                             var content = results[0].formatted_address;
+                     function (results, status) {
+                         if (status === google.maps.GeocoderStatus.OK) {
+                             // set formatted_address to later use on server-side.
                              model.set({formatted_address: results[0].formatted_address});
-                             content += "<br/>" + moment(model.get['timestamp']).format("MMM Do, h:mm:ss a");
                              self.infowindow.setPosition(location);
-                             self.infowindow.setContent(content);
+                             self.infowindow.setContent(
+                                 '<p>' + results[0].formatted_address + '</p>' + 
+                                     '<span>' + moment(model.get('timestamp').format("MMM Do, h:mm:ss a")) + '</span>');
                              self.infowindow.open(self.map);
                          } 
                      });
@@ -116,14 +99,14 @@
                  this.map.setZoom(14);
              },
 
-             render: function() {
+             render: function () {
                  var self = this;
                  var the_path = this.polyline.getPath();
                  this.collection.each(function(model, index) {
                      the_path.push(new google.maps.LatLng(model.get('lat'),  model.get('lng')));
                  });
                  this.polyline.setPath(the_path);
-
+                 // just render InfoWindow at the last point.
                  if (this.collection.length > 0) {
                      this.renderInfoWindow(this.collection.at(
                                            this.collection.length -1));
@@ -131,27 +114,30 @@
                  return this.el;
              },
                           
-             start_record: function() {
+             start_record: function () {
                  var self = this;
                  var model_dict = {};
+                 // Native 'watchPosition' is not working on my env.
                  this.updater = $.periodic(
                      function() {
                          navigator.geolocation.getCurrentPosition(
-                             function(position) {
-                                 if (position.coords.latitude != model_dict.lat && position.coords.longitude != model_dict.lng) {
+                             function (position) {
+                                 if (position.coords.latitude !== model_dict.lat && position.coords.longitude !== model_dict.lng) {
                                      model_dict = {lat: position.coords.latitude,
                                                    lng: position.coords.longitude,
                                                    speed: position.coords.longitude,
                                                    timestamp: position.timestamp};
                                      var model = self.collection.create(model_dict); 
-                                     if (self.collection.length == 1) {
+                                     if (self.collection.length === 1) {
                                          self.renderInfoWindow(model);
                                      }
                                  }
                              }, 
-                             function() {
+
+                             function () {
                                  //error handler here.
                              },
+
                              {
                                  enableHighAccuracy: true
                              }
@@ -159,10 +145,10 @@
                      });
              },
 
-
-             stop_record: function() {
+             stop_record: function () {
                  this.updater.cancel();
                  this.render();
+                 // do actual PUT to the server
                  this.model.save({points: this.collection.toJSON()});
                  this.collection.reset([]);
              }
@@ -170,12 +156,14 @@
          });
 
 
+     // Main App - entry point.
      window.TraceApp = Backbone.View.extend(
          {
-             initialize: function(){
+             initialize: function () {
                  var trace = new Trace({id: this.options.uuid});
                  var points = new Points(this.options.points);
                  var view = new TraceView({model: trace,
+                                           map: this.options.gmaps_app.map,
                                            collection: points,
                                            el: 'nav'});
                  view.render();
